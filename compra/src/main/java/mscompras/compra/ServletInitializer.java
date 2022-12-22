21dbcc7f17e7d247c3ac082a63a77290f65086b7
Samuel <samuel.encon@gmail.com>
2022-12-22 13:06:08 -0400

    first commit

---
+++ compra/src/main/java/mscompras/compra/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mscompras.compra;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CompraApplication.class);
+	}
+
+}
